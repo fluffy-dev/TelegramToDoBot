e5c170d99fb8ff323fe8c5bac562cb9fc3a1c584
Rauan <asetovrauan95@gmail.com>
2025-06-26 22:53:17 +0500

    user model

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class TelegramProfile(models.Model):
+    """
+    Links a Django User to a Telegram user ID.
+
+    Attributes:
+        user (OneToOneField): The Django user instance.
+        telegram_id (BigIntegerField): The unique identifier from Telegram.
+    """
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='telegram_profile'
+    )
+    telegram_id = models.BigIntegerField(unique=True)
+
+    def __str__(self):
+        """String representation of a TelegramProfile."""
+        return f"{self.user.username} - {self.telegram_id}"
