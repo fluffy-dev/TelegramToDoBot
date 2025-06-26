660d2a9e49d656dc588e39635b7cf52587d168fe
Rauan <asetovrauan95@gmail.com>
2025-06-26 20:44:10 +0500

    base setup

---
+++ src/common/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    """
+    An abstract base class model that provides self-updating
+    `created_at` and `updated_at` fields.
+    """
+    created_at = models.DateTimeField(db_index=True, default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
