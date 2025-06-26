50485c37bf4dd2f9c217dbc900f0dcf5ac48ba36
Rauan <asetovrauan95@gmail.com>
2025-06-26 20:45:26 +0500

    todo app: category model

--- src/todos/models.py
+++ src/todos/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class Category(BaseModel):
+    """
+    Represents a category (or tag) for a task.
+
+    Attributes:
+        id (CharField): The primary key, a 64-character hash.
+        name (CharField): The name of the category.
+        user (ForeignKey): The user who owns this category.
+    """
+    id = models.CharField(
+        primary_key=True,
+        max_length=64,
+        editable=False,
+    )
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='categories'
+    )
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        unique_together = ('user', 'name')
+
+    def __str__(self):
+        """String representation of a Category."""
+        return self.name
