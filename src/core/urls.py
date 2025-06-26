7703d28ae56f8a52956a5cbdd57aae266c70b24d
Rauan <asetovrauan95@gmail.com>
2025-06-26 22:55:32 +0500

    added user urls to main urls

--- src/core/urls.py
+++ src/core/urls.py
@@ -21,6 +21,7 @@ from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/users/', include(('users.urls', 'users'))),
     path('api/v1/', include(('todos.urls', 'todos'))),
     path('api-token-auth/', views.obtain_auth_token)
 ]
